ecb94937f6ef23bc212b789eb16148e42736a01c
Fixitfetish <Fixitfetish@github>
2017-05-03 23:51:35 +0200

    updated fifo

--- VHDL/ramlib/fifo_sync.behave.vhdl
+++ VHDL/ramlib/fifo_sync.behave.vhdl
@@ -1,39 +1,20 @@
 -------------------------------------------------------------------------------
--- FILE    : fifo_sync_behave.vhdl   
--- AUTHOR  : Fixitfetish
--- DATE    : 07/May/2016
--- VERSION : 1.0
--- VHDL    : 1993
--- LICENSE : MIT License
--------------------------------------------------------------------------------
--- Copyright (c) 2016 Fixitfetish
--- 
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- 
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
--- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
--- IN THE SOFTWARE.
+--! @file       fifo_sync.behave.vhdl
+--! @author     Fixitfetish
+--! @date       07/May/2016
+--! @version    1.00
+--! @copyright  MIT License
+--! @note       VHDL-1993
 -------------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
----------------------------------------------------------------------------------------------------
--- Note: The behavioral model shall only be used in simulations. For synthesis please use a FPGA
--- specific implementation which is typically more efficient in terms of resources and timing.
----------------------------------------------------------------------------------------------------
+--! @brief The behavioral model shall only be used in simulations.
+--! 
+--! For synthesis please use a FPGA specific implementation which is typically
+--! more efficient in terms of resources and timing.
+
 architecture behave of fifo_sync is
 
   -- FIFO
@@ -136,7 +117,8 @@ begin
   end generate;
   
   g_req : if not ACKNOWLEDGE_MODE generate
-   process(clock)
+  begin
+   p_out : process(clock)
    begin
      if rising_edge(clock) then
        if reset='1' then
--- VHDL/ramlib/fifo_sync.vhdl
+++ VHDL/ramlib/fifo_sync.vhdl
@@ -1,68 +1,65 @@
 -------------------------------------------------------------------------------
--- FILE    : fifo_sync.vhdl   
--- AUTHOR  : Fixitfetish
--- DATE    : 07/May/2016
--- VERSION : 1.0
--- VHDL    : 1993
--- LICENSE : MIT License
--------------------------------------------------------------------------------
--- Copyright (c) 2016 Fixitfetish
--- 
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- 
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
--- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
--- IN THE SOFTWARE.
+--! @file       fifo_sync.vhdl
+--! @author     Fixitfetish
+--! @date       07/May/2016
+--! @version    1.00
+--! @copyright  MIT License
+--! @note       VHDL-1993
 -------------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
----------------------------------------------------------------------------------------------------
--- The synchronous FIFO supports two modes:
---   REQUEST MODE = If the FIFO is not empty data can be requested and appears at the data output
---     port one cycles after the request. 
---   ACKNOWLEDGE MODE = If the FIFO is not empty valid data is present at the data output port
---     and must be acknowledged before the next data is passed to the output. This mode is also
---     known First-Word-Fall-Through (FWFT).
----------------------------------------------------------------------------------------------------
+--! @brief Synchronous FIFO that supports request mode and acknowledge mode.
+--!
+--! * REQUEST MODE = If the FIFO is not empty then data can be requested and
+--!   appears at the data output port one cycle after the request. 
+--! * ACKNOWLEDGE MODE = If the FIFO is not empty then valid data is present at
+--!   the data output port and must be acknowledged before the next data is passed
+--!   to the output. This mode is also known First-Word-Fall-Through (FWFT).
+
 entity fifo_sync is
 generic (
-  FIFO_WIDTH : positive; -- data width in bits (mandatory!)
-  FIFO_DEPTH : positive; -- FIFO depth in number of data words (mandatory!)
-  USE_BLOCK_RAM : boolean := false; -- true=use vendor specific block ram type, false=don't use block ram but logic
-  ACKNOWLEDGE_MODE : boolean := false; -- false=read request, true=read acknowledge (fall-through, show-ahead)
-  ALMOST_FULL_THRESHOLD : natural := 0; -- 0(unused) < almost full threshold < FIFO_DEPTH
-  ALMOST_EMPTY_THRESHOLD : natural := 0  -- 0(unused) < almost empty threshold < FIFO_DEPTH
+  --! Data width in bits (mandatory!)
+  FIFO_WIDTH : positive;
+  --! FIFO depth in number of data words (mandatory!)
+  FIFO_DEPTH : positive;
+  --! true=use vendor specific block ram type, false=don't use block ram but logic
+  USE_BLOCK_RAM : boolean := false;
+  --! false=read request, true=read acknowledge (fall-through, show-ahead)
+  ACKNOWLEDGE_MODE : boolean := false;
+  --! 0(unused) < almost full threshold < FIFO_DEPTH
+  ALMOST_FULL_THRESHOLD : natural := 0;
+  --! 0(unused) < almost empty threshold < FIFO_DEPTH
+  ALMOST_EMPTY_THRESHOLD : natural := 0
 );
 port (
-  clock        : in  std_logic; -- read/write clock
-  reset        : in  std_logic; -- synchrounous reset
-  level        : out integer range 0 to FIFO_DEPTH; -- FIFO fill level
-  -- write port
-  wr_ena       : in  std_logic; -- write data enable
-  wr_din       : in  std_logic_vector(FIFO_WIDTH-1 downto 0); -- write data input
-  wr_full      : out std_logic; -- FIFO full
-  wr_alm_full  : out std_logic; -- FIFO almost full
-  wr_overflow  : out std_logic; -- FIFO overflow (wr_ena=1 and wr_full=1)
-  -- read port
-  rd_req_ack   : in  std_logic; -- read request/acknowledge
-  rd_dout      : out std_logic_vector(FIFO_WIDTH-1 downto 0); -- read data output
-  rd_empty     : out std_logic; -- FIFO empty
-  rd_alm_empty : out std_logic; -- FIFO almost empty
-  rd_underflow : out std_logic  -- FIFO underflow (rd_req_ack=1 and rd_empty=1)
+  --! Clock for read and write port
+  clock        : in  std_logic;
+  --! Synchronous reset
+  reset        : in  std_logic;
+  --! FIFO fill level
+  level        : out integer range 0 to FIFO_DEPTH;
+  --! Write data enable
+  wr_ena       : in  std_logic;
+  --! Write data input
+  wr_din       : in  std_logic_vector(FIFO_WIDTH-1 downto 0);
+  --! FIFO full
+  wr_full      : out std_logic;
+  --! FIFO almost full
+  wr_alm_full  : out std_logic;
+  --! FIFO overflow (wr_ena=1 and wr_full=1)
+  wr_overflow  : out std_logic;
+  --! Read request/acknowledge
+  rd_req_ack   : in  std_logic;
+  --! Read data output
+  rd_dout      : out std_logic_vector(FIFO_WIDTH-1 downto 0);
+  --! FIFO empty
+  rd_empty     : out std_logic;
+  --! FIFO almost empty
+  rd_alm_empty : out std_logic;
+  --! FIFO underflow (rd_req_ack=1 and rd_empty=1)
+  rd_underflow : out std_logic
 );
 begin
   ASSERT ALMOST_FULL_THRESHOLD<FIFO_DEPTH
