cb616b36280ef0d3c706b2929803c226c856353b
Fixitfetish <Fixitfetish@github>
2017-06-05 22:53:58 +0200

    cplx_mult test

---
+++ VHDL_testbench/cplxlib/cplx_mult/mult.m
@@ -0,0 +1,79 @@
+% mult example to test the entity "cplx_mult"
+% element-wise complex vector multiplication
+
+fname_x_stimuli = 'mult_x_sti.txt';
+fname_y_stimuli = 'mult_y_sti.txt';
+fname_result = 'mult_log.txt';
+
+% number of cycles (number of rows)
+cycles = 19;
+
+% length of complex vector X, i.e. number of columns
+LX = 5;
+
+% length of complex vector Y, i.e. number of columns
+% (must be 1 or same length as X)
+LY = 5;
+
+% stimuli
+rand("seed",1); % use always same seed 
+x_data = (1+1i) - 2*(rand(cycles,LX) + 1i*rand(cycles,LX));
+y_data = (1+1i) - 2*(rand(cycles,LY) + 1i*rand(cycles,LY));
+
+addpath('../');
+
+% create CPLX stimuli file
+x_sti = cplx_interface(18,'frac');
+x_sti = x_sti.appendReset(3,LX);
+x_sti = x_sti.appendData(x_data);
+x_sti = x_sti.appendInvalid(10);
+x_sti.writeFile(fname_x_stimuli);
+
+y_sti = cplx_interface(18,'frac');
+y_sti = y_sti.appendReset(3,LY);
+y_sti = y_sti.appendData(y_data);
+y_sti = y_sti.appendInvalid(10);
+y_sti.writeFile(fname_y_stimuli);
+
+disp(['Stimuli files "',fname_x_stimuli,'" and "',fname_y_stimuli,'" have been generated.'])
+disp('Please run VHDL simulation.');
+disp(['Press any key to start evaluation of simulation result file "',fname_result,'".']);
+pause
+
+
+% Evaluate CPLX result file
+res = cplx_interface(18,'int');
+res = res.readFile(fname_result);
+n_ovf = sum(sum(res.cplx.ovf(:,1:LX)));
+disp(['Number of overflows detected: ',num2str(n_ovf)]);
+
+% determine delay
+delay = find(res.cplx.vld(:,1),1) - find(res.cplx.vld(:,LX+1),1);
+disp(['Multiplier delay [cycles]: ',num2str(delay)]);
+
+% extract valid result values (only result vector without delay reference)
+R = res.cplx.data(:,1:LX);
+R = R(logical(res.cplx.vld(:,1:LX)));
+R = reshape(R,cycles,[]);
+
+% reference
+if LY==1,
+  ref = x_data .* repmat(y_data,1,LX);
+else
+  ref = x_data .* y_data;
+end
+% convert to integer and consider one additional MSB guard bit
+ref = (2^17) * ref / 2; 
+
+% determine error 
+err = R - ref;
+max_err_re = max(max(abs(real(err))));
+max_err_im = max(max(abs(imag(err))));
+disp(['Maximum absolute error [integer diff]:  Re=',num2str(max_err_re),'  Im=',num2str(max_err_im)]);
+
+if (max_err_re<1.5) && (max_err_im<1.5),
+  % threshold 1.5 works independent of rounding enable/disable
+  disp('OK: Difference is as expected, i.e. below 1.5 .');
+else  
+  disp('ERROR: Difference too big - check settings.');
+end
---
+++ VHDL_testbench/cplxlib/cplx_mult/mult_tb.vhdl
@@ -0,0 +1,139 @@
+-------------------------------------------------------------------------------
+-- FILE    : mult_tb.vhdl
+-- AUTHOR  : Fixitfetish
+-- DATE    : 06/Jun/2017
+-- VERSION : 0.30
+-- VHDL    : 1993
+-- LICENSE : MIT License
+-------------------------------------------------------------------------------
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+library cplxlib;
+  use cplxlib.cplx_pkg.all;
+
+use std.textio.all;
+
+entity mult_tb is
+end entity;
+
+architecture sim of mult_tb is
+  
+  constant PERIOD : time := 1 ns; -- 1000MHz
+  constant LX : natural := 5;  -- vector length x
+  constant LY : natural := 5;  -- vector length y
+  constant LR : natural := LX; -- vector length result
+
+  constant FILENAME_X : string := "mult_x_sti.txt"; -- input x
+  constant FILENAME_Y : string := "mult_y_sti.txt"; -- input y
+  constant FILENAME_R : string := "mult_log.txt"; -- result
+
+  signal rst : std_logic := '1';
+  signal clk : std_logic := '1';
+  signal clkena : std_logic := '0';
+  signal finish : std_logic := '0';
+
+  signal x : cplx_vector(0 to LX-1) := cplx_vector_reset(18,LX,"R");
+  signal y : cplx_vector(0 to LY-1) := cplx_vector_reset(18,LY,"R");
+  signal r : cplx_vector(0 to LR-1);
+
+begin
+
+  p_clk : process
+  begin
+    while finish='0' loop
+      wait for PERIOD/2;
+      clk <= not clk;
+    end loop;
+    -- epilog, 5 cycles
+    for n in 1 to 10 loop
+      wait for PERIOD/2;
+      clk <= not clk;
+    end loop;
+    report "INFO: Clock stopped. End of simulation." severity note;
+    wait; -- stop clock
+  end process;
+
+  -- release reset
+  rst <= '0' after 2*PERIOD;
+
+  p_start : process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst='1' then
+        clkena <= '0';
+      else
+        clkena <= not clkena;
+      end if;
+    end if;
+  end process;
+
+  x_sti : entity work.cplx_stimuli
+  generic map(
+    NUM_CPLX => LX,
+    SKIP_PRECEDING_LINES => 2,
+    GEN_DECIMAL => true,
+    GEN_INVALID => true,
+    GEN_FILE => FILENAME_X
+  )
+  port map (
+    rst     => rst,
+    clk     => clk,
+    clkena  => clkena,
+    dout    => x,
+    finish  => finish
+  );
+
+  y_sti : entity work.cplx_stimuli
+  generic map(
+    NUM_CPLX => LY,
+    SKIP_PRECEDING_LINES => 2,
+    GEN_DECIMAL => true,
+    GEN_INVALID => true,
+    GEN_FILE => FILENAME_Y
+  )
+  port map (
+    rst     => rst,
+    clk     => clk,
+    clkena  => clkena,
+    dout    => y,
+    finish  => finish
+  );
+
+  I1 : entity cplxlib.cplx_mult
+  generic map(
+    NUM_MULT              => LX, -- number of parallel multiplications
+    HIGH_SPEED_MODE       => false,  -- enable high speed mode
+    NUM_INPUT_REG         => 3,  -- number of input registers
+    NUM_OUTPUT_REG        => 4,  -- number of output registers
+    INPUT_OVERFLOW_IGNORE => false,  -- ignore input overflows
+    OUTPUT_SHIFT_RIGHT    => 18,  -- number of right shifts
+    MODE                  => "NOS" -- options
+  )
+  port map(
+    clk        => clk  , -- clock
+    clk2       => open  , -- clock x2
+    neg        => (others=>'0'), -- negation per input x
+    x          => x, -- first factors
+    y          => y  , -- second factors
+    result     => r, -- product results
+    PIPESTAGES => open  -- constant number of pipeline stages
+  );
+
+  i_log : entity work.cplx_logger
+  generic map(
+    NUM_CPLX => LR+1,
+    LOG_DECIMAL => true,
+    LOG_INVALID => true,
+    LOG_FILE => FILENAME_R,
+    TITLE => "MULT_OUT"
+  )
+  port map (
+    clk     => clk,
+    rst     => rst,
+    din(0 to LR-1) => r,
+    din(LR) => x(0), -- input reference for delay check
+    finish  => finish
+  );
+
+end architecture;
