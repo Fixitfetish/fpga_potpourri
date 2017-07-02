0571ff0afbb879422acfe7c12e638225bd4787a4
Fixitfetish <Fixitfetish@github>
2017-07-02 22:45:30 +0200

    more types and license link

--- VHDL/baselib/ieee_extension_types_1993.vhdl
+++ VHDL/baselib/ieee_extension_types_1993.vhdl
@@ -3,8 +3,11 @@
 --! @author     Fixitfetish
 --! @date       01/May/2017
 --! @version    0.15
---! @copyright  MIT License
 --! @note       VHDL-1993
+--! @copyright  <https://en.wikipedia.org/wiki/MIT_License> ,
+--!             <https://opensource.org/licenses/MIT>
+-------------------------------------------------------------------------------
+-- Includes DOXYGEN support.
 -------------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -29,27 +32,49 @@ package ieee_extension_types is
   -----------------------------------------------------------------------------
   -- UNSIGNED vector types
   type unsigned4_vector  is array(integer range<>) of unsigned( 3 downto 0);
+  type unsigned5_vector  is array(integer range<>) of unsigned( 4 downto 0);
   type unsigned6_vector  is array(integer range<>) of unsigned( 5 downto 0);
+  type unsigned7_vector  is array(integer range<>) of unsigned( 6 downto 0);
   type unsigned8_vector  is array(integer range<>) of unsigned( 7 downto 0);
+  type unsigned9_vector  is array(integer range<>) of unsigned( 8 downto 0);
   type unsigned10_vector is array(integer range<>) of unsigned( 9 downto 0);
+  type unsigned11_vector is array(integer range<>) of unsigned(10 downto 0);
   type unsigned12_vector is array(integer range<>) of unsigned(11 downto 0);
+  type unsigned13_vector is array(integer range<>) of unsigned(12 downto 0);
   type unsigned14_vector is array(integer range<>) of unsigned(13 downto 0);
+  type unsigned15_vector is array(integer range<>) of unsigned(14 downto 0);
   type unsigned16_vector is array(integer range<>) of unsigned(15 downto 0);
+  type unsigned17_vector is array(integer range<>) of unsigned(16 downto 0);
   type unsigned18_vector is array(integer range<>) of unsigned(17 downto 0);
+  type unsigned19_vector is array(integer range<>) of unsigned(18 downto 0);
   type unsigned20_vector is array(integer range<>) of unsigned(19 downto 0);
+  type unsigned21_vector is array(integer range<>) of unsigned(20 downto 0);
   type unsigned22_vector is array(integer range<>) of unsigned(21 downto 0);
+  type unsigned23_vector is array(integer range<>) of unsigned(22 downto 0);
   type unsigned24_vector is array(integer range<>) of unsigned(23 downto 0);
+  type unsigned25_vector is array(integer range<>) of unsigned(24 downto 0);
   type unsigned26_vector is array(integer range<>) of unsigned(25 downto 0);
+  type unsigned27_vector is array(integer range<>) of unsigned(26 downto 0);
   type unsigned28_vector is array(integer range<>) of unsigned(27 downto 0);
+  type unsigned29_vector is array(integer range<>) of unsigned(28 downto 0);
   type unsigned30_vector is array(integer range<>) of unsigned(29 downto 0);
+  type unsigned31_vector is array(integer range<>) of unsigned(30 downto 0);
   type unsigned32_vector is array(integer range<>) of unsigned(31 downto 0);
+  type unsigned33_vector is array(integer range<>) of unsigned(32 downto 0);
   type unsigned34_vector is array(integer range<>) of unsigned(33 downto 0);
+  type unsigned35_vector is array(integer range<>) of unsigned(34 downto 0);
   type unsigned36_vector is array(integer range<>) of unsigned(35 downto 0);
+  type unsigned37_vector is array(integer range<>) of unsigned(36 downto 0);
   type unsigned38_vector is array(integer range<>) of unsigned(37 downto 0);
+  type unsigned39_vector is array(integer range<>) of unsigned(38 downto 0);
   type unsigned40_vector is array(integer range<>) of unsigned(39 downto 0);
+  type unsigned41_vector is array(integer range<>) of unsigned(40 downto 0);
   type unsigned42_vector is array(integer range<>) of unsigned(41 downto 0);
+  type unsigned43_vector is array(integer range<>) of unsigned(42 downto 0);
   type unsigned44_vector is array(integer range<>) of unsigned(43 downto 0);
+  type unsigned45_vector is array(integer range<>) of unsigned(44 downto 0);
   type unsigned46_vector is array(integer range<>) of unsigned(45 downto 0);
+  type unsigned47_vector is array(integer range<>) of unsigned(46 downto 0);
   type unsigned48_vector is array(integer range<>) of unsigned(47 downto 0);
 
   --! default standard unsigned vector type
@@ -58,27 +83,49 @@ package ieee_extension_types is
   -----------------------------------------------------------------------------
   -- SIGNED vector types
   type signed4_vector  is array(integer range<>) of signed( 3 downto 0);
+  type signed5_vector  is array(integer range<>) of signed( 4 downto 0);
   type signed6_vector  is array(integer range<>) of signed( 5 downto 0);
+  type signed7_vector  is array(integer range<>) of signed( 6 downto 0);
   type signed8_vector  is array(integer range<>) of signed( 7 downto 0);
+  type signed9_vector  is array(integer range<>) of signed( 8 downto 0);
   type signed10_vector is array(integer range<>) of signed( 9 downto 0);
+  type signed11_vector is array(integer range<>) of signed(10 downto 0);
   type signed12_vector is array(integer range<>) of signed(11 downto 0);
+  type signed13_vector is array(integer range<>) of signed(12 downto 0);
   type signed14_vector is array(integer range<>) of signed(13 downto 0);
+  type signed15_vector is array(integer range<>) of signed(14 downto 0);
   type signed16_vector is array(integer range<>) of signed(15 downto 0);
+  type signed17_vector is array(integer range<>) of signed(16 downto 0);
   type signed18_vector is array(integer range<>) of signed(17 downto 0);
+  type signed19_vector is array(integer range<>) of signed(18 downto 0);
   type signed20_vector is array(integer range<>) of signed(19 downto 0);
+  type signed21_vector is array(integer range<>) of signed(20 downto 0);
   type signed22_vector is array(integer range<>) of signed(21 downto 0);
+  type signed23_vector is array(integer range<>) of signed(22 downto 0);
   type signed24_vector is array(integer range<>) of signed(23 downto 0);
+  type signed25_vector is array(integer range<>) of signed(24 downto 0);
   type signed26_vector is array(integer range<>) of signed(25 downto 0);
+  type signed27_vector is array(integer range<>) of signed(26 downto 0);
   type signed28_vector is array(integer range<>) of signed(27 downto 0);
+  type signed29_vector is array(integer range<>) of signed(28 downto 0);
   type signed30_vector is array(integer range<>) of signed(29 downto 0);
+  type signed31_vector is array(integer range<>) of signed(30 downto 0);
   type signed32_vector is array(integer range<>) of signed(31 downto 0);
+  type signed33_vector is array(integer range<>) of signed(32 downto 0);
   type signed34_vector is array(integer range<>) of signed(33 downto 0);
+  type signed35_vector is array(integer range<>) of signed(34 downto 0);
   type signed36_vector is array(integer range<>) of signed(35 downto 0);
+  type signed37_vector is array(integer range<>) of signed(36 downto 0);
   type signed38_vector is array(integer range<>) of signed(37 downto 0);
+  type signed39_vector is array(integer range<>) of signed(38 downto 0);
   type signed40_vector is array(integer range<>) of signed(39 downto 0);
+  type signed41_vector is array(integer range<>) of signed(40 downto 0);
   type signed42_vector is array(integer range<>) of signed(41 downto 0);
+  type signed43_vector is array(integer range<>) of signed(42 downto 0);
   type signed44_vector is array(integer range<>) of signed(43 downto 0);
+  type signed45_vector is array(integer range<>) of signed(44 downto 0);
   type signed46_vector is array(integer range<>) of signed(45 downto 0);
+  type signed47_vector is array(integer range<>) of signed(46 downto 0);
   type signed48_vector is array(integer range<>) of signed(47 downto 0);
 
   --! default standard signed vector type
--- VHDL/baselib/ieee_extension_types_2008.vhdl
+++ VHDL/baselib/ieee_extension_types_2008.vhdl
@@ -3,8 +3,11 @@
 --! @author     Fixitfetish
 --! @date       17/Apr/2017
 --! @version    0.11
---! @copyright  MIT License
 --! @note       VHDL-2008
+--! @copyright  <https://en.wikipedia.org/wiki/MIT_License> ,
+--!             <https://opensource.org/licenses/MIT>
+-------------------------------------------------------------------------------
+-- Includes DOXYGEN support.
 -------------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -21,51 +24,95 @@ package ieee_extension_types is
   type signed_vector is array(integer range <>) of signed;
 
   subtype unsigned4_vector  is unsigned_vector(open)( 3 downto 0);
+  subtype unsigned5_vector  is unsigned_vector(open)( 4 downto 0);
   subtype unsigned6_vector  is unsigned_vector(open)( 5 downto 0);
+  subtype unsigned7_vector  is unsigned_vector(open)( 6 downto 0);
   subtype unsigned8_vector  is unsigned_vector(open)( 7 downto 0);
+  subtype unsigned9_vector  is unsigned_vector(open)( 8 downto 0);
   subtype unsigned10_vector is unsigned_vector(open)( 9 downto 0);
+  subtype unsigned11_vector is unsigned_vector(open)(10 downto 0);
   subtype unsigned12_vector is unsigned_vector(open)(11 downto 0);
+  subtype unsigned13_vector is unsigned_vector(open)(12 downto 0);
   subtype unsigned14_vector is unsigned_vector(open)(13 downto 0);
+  subtype unsigned15_vector is unsigned_vector(open)(14 downto 0);
   subtype unsigned16_vector is unsigned_vector(open)(15 downto 0);
+  subtype unsigned17_vector is unsigned_vector(open)(16 downto 0);
   subtype unsigned18_vector is unsigned_vector(open)(17 downto 0);
+  subtype unsigned19_vector is unsigned_vector(open)(18 downto 0);
   subtype unsigned20_vector is unsigned_vector(open)(19 downto 0);
+  subtype unsigned21_vector is unsigned_vector(open)(20 downto 0);
   subtype unsigned22_vector is unsigned_vector(open)(21 downto 0);
+  subtype unsigned23_vector is unsigned_vector(open)(22 downto 0);
   subtype unsigned24_vector is unsigned_vector(open)(23 downto 0);
+  subtype unsigned25_vector is unsigned_vector(open)(24 downto 0);
   subtype unsigned26_vector is unsigned_vector(open)(25 downto 0);
+  subtype unsigned27_vector is unsigned_vector(open)(26 downto 0);
   subtype unsigned28_vector is unsigned_vector(open)(27 downto 0);
+  subtype unsigned29_vector is unsigned_vector(open)(28 downto 0);
   subtype unsigned30_vector is unsigned_vector(open)(29 downto 0);
+  subtype unsigned31_vector is unsigned_vector(open)(30 downto 0);
   subtype unsigned32_vector is unsigned_vector(open)(31 downto 0);
+  subtype unsigned33_vector is unsigned_vector(open)(32 downto 0);
   subtype unsigned34_vector is unsigned_vector(open)(33 downto 0);
+  subtype unsigned35_vector is unsigned_vector(open)(34 downto 0);
   subtype unsigned36_vector is unsigned_vector(open)(35 downto 0);
+  subtype unsigned37_vector is unsigned_vector(open)(36 downto 0);
   subtype unsigned38_vector is unsigned_vector(open)(37 downto 0);
+  subtype unsigned39_vector is unsigned_vector(open)(38 downto 0);
   subtype unsigned40_vector is unsigned_vector(open)(39 downto 0);
+  subtype unsigned41_vector is unsigned_vector(open)(40 downto 0);
   subtype unsigned42_vector is unsigned_vector(open)(41 downto 0);
+  subtype unsigned43_vector is unsigned_vector(open)(42 downto 0);
   subtype unsigned44_vector is unsigned_vector(open)(43 downto 0);
+  subtype unsigned45_vector is unsigned_vector(open)(44 downto 0);
   subtype unsigned46_vector is unsigned_vector(open)(45 downto 0);
+  subtype unsigned47_vector is unsigned_vector(open)(46 downto 0);
   subtype unsigned48_vector is unsigned_vector(open)(47 downto 0);
 
   subtype signed4_vector  is signed_vector(open)( 3 downto 0);
+  subtype signed5_vector  is signed_vector(open)( 4 downto 0);
   subtype signed6_vector  is signed_vector(open)( 5 downto 0);
+  subtype signed7_vector  is signed_vector(open)( 6 downto 0);
   subtype signed8_vector  is signed_vector(open)( 7 downto 0);
+  subtype signed9_vector  is signed_vector(open)( 8 downto 0);
   subtype signed10_vector is signed_vector(open)( 9 downto 0);
+  subtype signed11_vector is signed_vector(open)(10 downto 0);
   subtype signed12_vector is signed_vector(open)(11 downto 0);
+  subtype signed13_vector is signed_vector(open)(12 downto 0);
   subtype signed14_vector is signed_vector(open)(13 downto 0);
+  subtype signed15_vector is signed_vector(open)(14 downto 0);
   subtype signed16_vector is signed_vector(open)(15 downto 0);
+  subtype signed17_vector is signed_vector(open)(16 downto 0);
   subtype signed18_vector is signed_vector(open)(17 downto 0);
+  subtype signed19_vector is signed_vector(open)(18 downto 0);
   subtype signed20_vector is signed_vector(open)(19 downto 0);
+  subtype signed21_vector is signed_vector(open)(20 downto 0);
   subtype signed22_vector is signed_vector(open)(21 downto 0);
+  subtype signed23_vector is signed_vector(open)(22 downto 0);
   subtype signed24_vector is signed_vector(open)(23 downto 0);
+  subtype signed25_vector is signed_vector(open)(24 downto 0);
   subtype signed26_vector is signed_vector(open)(25 downto 0);
+  subtype signed27_vector is signed_vector(open)(26 downto 0);
   subtype signed28_vector is signed_vector(open)(27 downto 0);
+  subtype signed29_vector is signed_vector(open)(28 downto 0);
   subtype signed30_vector is signed_vector(open)(29 downto 0);
+  subtype signed31_vector is signed_vector(open)(30 downto 0);
   subtype signed32_vector is signed_vector(open)(31 downto 0);
+  subtype signed33_vector is signed_vector(open)(32 downto 0);
   subtype signed34_vector is signed_vector(open)(33 downto 0);
+  subtype signed35_vector is signed_vector(open)(34 downto 0);
   subtype signed36_vector is signed_vector(open)(35 downto 0);
+  subtype signed37_vector is signed_vector(open)(36 downto 0);
   subtype signed38_vector is signed_vector(open)(37 downto 0);
+  subtype signed39_vector is signed_vector(open)(38 downto 0);
   subtype signed40_vector is signed_vector(open)(39 downto 0);
+  subtype signed41_vector is signed_vector(open)(40 downto 0);
   subtype signed42_vector is signed_vector(open)(41 downto 0);
+  subtype signed43_vector is signed_vector(open)(42 downto 0);
   subtype signed44_vector is signed_vector(open)(43 downto 0);
+  subtype signed45_vector is signed_vector(open)(44 downto 0);
   subtype signed46_vector is signed_vector(open)(45 downto 0);
+  subtype signed47_vector is signed_vector(open)(46 downto 0);
   subtype signed48_vector is signed_vector(open)(47 downto 0);
 
 end package;
