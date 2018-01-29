963a0a5c9000bb30450d046d2b4c3cde1133208b
Fixitfetish <Fixitfetish@github>
2018-01-29 22:58:46 +0100

    first draft of asynchronous FIFO

---
+++ VHDL/ramlib/behave/fifo_async.behave.vhdl
@@ -0,0 +1,140 @@
+-------------------------------------------------------------------------------
+--! @file       fifo_async.behave.vhdl
+--! @author     Fixitfetish
+--! @date       07/May/2016
+--! @version    1.00
+--! @note       VHDL-1993
+--! @copyright  <https://en.wikipedia.org/wiki/MIT_License> ,
+--!             <https://opensource.org/licenses/MIT>
+-------------------------------------------------------------------------------
+-- Includes DOXYGEN support.
+-------------------------------------------------------------------------------
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+--! @brief The behavioral model shall only be used in simulations.
+--! THIS IS ONLY A NON-WORKING FIRST DRAFT VERSION !!!
+--! 
+--! For synthesis please use a FPGA specific implementation which is typically
+--! more efficient in terms of resources and timing.
+
+architecture behave of fifo_async is
+
+  type ram_type is array (1 to FIFO_DEPTH) of std_logic_vector(FIFO_WIDTH-1 downto 0);
+  signal fifo : ram_type := (others=>(others=>'0'));
+  signal async_rst : std_logic; -- asynchronous reset (read and write port) 
+  
+  -- write clock
+  signal wr_rst : std_logic; -- write port synchronous reset 
+  signal wr_pointer : integer range 1 to FIFO_DEPTH;
+  signal wr_level : integer range 0 to FIFO_DEPTH;
+  signal wr_read : std_logic;
+  signal full : std_logic;
+  signal fifo_wr : std_logic;
+
+  -- read clock
+  signal rd_rst : std_logic; -- read port synchronous reset 
+  signal rd_pointer : integer range 1 to FIFO_DEPTH;
+  signal rd_level : integer range 0 to FIFO_DEPTH;
+  signal rd_write : std_logic;
+  signal empty : std_logic;
+  signal fifo_rd : std_logic;
+
+begin
+
+  -- reset
+  g_arst : if USE_ASYNC_RESET generate
+    async_rst <= reset;
+    wr_rst <= '0';
+    rd_rst <= '0';
+  end generate;
+  g_srst : if not USE_ASYNC_RESET generate
+    signal wr_srst : std_logic_vector(1 to 2);
+    signal rd_srst : std_logic_vector(1 to 2);
+  begin
+    async_rst <= '0';
+    -- synchronizer chain for write port reset
+    process(wr_clk) begin if rising_edge(wr_clk) then
+      wr_srst(1)<=reset; wr_srst(2)<=wr_srst(1); wr_rst<=wr_srst(2); 
+    end if; end process; 
+    -- synchronizer chain for read port reset
+    process(rd_clk) begin if rising_edge(rd_clk) then
+      rd_srst(1)<=reset; rd_srst(2)<=rd_srst(1); rd_rst<=rd_srst(2); 
+    end if; end process; 
+  end generate;
+
+  full <= '1' when (wr_level=FIFO_DEPTH) else '0';
+  
+  p_write : process(wr_clk, async_rst)
+  begin
+    if async_rst='1' then
+      wr_pointer <= 1; 
+      wr_level <= 0;
+      fifo_wr <= '0';
+      fifo <= (others=>(others=>'0'));
+    elsif rising_edge(wr_clk) then
+      fifo_wr <= '0'; -- by default no FIFO write
+      if wr_rst='1' then
+        wr_pointer <= 1; 
+        wr_level <= 0;
+        fifo <= (others=>(others=>'0'));
+      else
+        if wr_ena='1' and (full='0' or wr_read='1') then
+          -- write to FIFO
+          fifo_wr <= '1';
+          fifo(wr_pointer) <= wr_din;
+          if wr_pointer=FIFO_DEPTH then 
+            wr_pointer <= 1; -- cyclic RAM wrap-around
+          else
+            wr_pointer <= wr_pointer + 1;
+          end if;
+          -- increase write level only when not read in same cycle
+          if wr_read='0' then 
+            wr_level <= wr_level + 1;
+          end if; 
+        elsif wr_ena='0' and wr_read='1' then
+          -- just read
+          wr_level <= wr_level - 1;
+        end if;
+      end if;
+      -- synchronizer
+      wr_read <= fifo_rd;
+    end if;  
+  end process;
+
+  wr_full <= full;
+  wr_alm_full <= '0' when (ALMOST_FULL_THRESHOLD=0) else
+                 '1' when (wr_level>=ALMOST_FULL_THRESHOLD) else '0';
+  wr_overflow <= full and wr_ena;
+  
+  ----------------------------------------------------------
+  
+  empty <= '1' when (rd_level=0) else '0';
+ 
+  p_read : process(rd_clk, async_rst)
+  begin
+    if async_rst='1' then
+      rd_pointer <= 1; 
+      rd_level <= 0;
+      fifo_rd <= '0';
+    elsif rising_edge(rd_clk) then
+      rd_rst <= wr_rst; -- synchronizer
+      fifo_rd <= '0'; -- by default no FIFO read
+      if rd_rst='1' then
+        rd_pointer <= 1; 
+        rd_level <= 0;
+      else
+      end if;  
+      -- synchronizer
+      rd_rst <= wr_rst;
+      rd_write <= fifo_wr;
+    end if;  
+  end process;
+
+  rd_empty <= empty;
+  rd_alm_empty <= '0' when (ALMOST_EMPTY_THRESHOLD=0) else
+                  '1' when (rd_level<=ALMOST_EMPTY_THRESHOLD) else '0';
+  rd_underflow <= empty and rd_req_ack;
+  
+end architecture;
---
+++ VHDL/ramlib/fifo_async.vhdl
@@ -0,0 +1,76 @@
+-------------------------------------------------------------------------------
+--! @file       fifo_async.vhdl
+--! @author     Fixitfetish
+--! @date       07/May/2016
+--! @version    1.00
+--! @note       VHDL-1993
+--! @copyright  <https://en.wikipedia.org/wiki/MIT_License> ,
+--!             <https://opensource.org/licenses/MIT>
+-------------------------------------------------------------------------------
+-- Includes DOXYGEN support.
+-------------------------------------------------------------------------------
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+--! @brief Asynchronous FIFO that supports request mode and acknowledge mode.
+--!
+--! * REQUEST MODE = If the FIFO is not empty then data can be requested and
+--!   appears at the data output port one cycle after the request. 
+--! * ACKNOWLEDGE MODE = If the FIFO is not empty then valid data is present at
+--!   the data output port and must be acknowledged before the next data is passed
+--!   to the output. This mode is also known First-Word-Fall-Through (FWFT).
+
+entity fifo_async is
+generic (
+  --! Data width in bits (mandatory!)
+  FIFO_WIDTH : positive;
+  --! FIFO depth in number of data words (mandatory!)
+  FIFO_DEPTH : positive;
+  --! false=synchronize reset to both clock domains, true=use asynchronous reset
+  USE_ASYNC_RESET : boolean := false;
+  --! true=use vendor specific block ram type, false=don't use block ram but logic
+  USE_BLOCK_RAM : boolean := false;
+  --! false=read request, true=read acknowledge (fall-through, show-ahead)
+  ACKNOWLEDGE_MODE : boolean := false;
+  --! 0(unused) < almost full threshold < FIFO_DEPTH
+  ALMOST_FULL_THRESHOLD : natural := 0;
+  --! 0(unused) < almost empty threshold < FIFO_DEPTH
+  ALMOST_EMPTY_THRESHOLD : natural := 0
+);
+port (
+  --! Reset (see generic USE_ASYNC_RESET)
+  reset        : in  std_logic;
+  --! Write clock
+  wr_clk       : in  std_logic;
+  --! Write data enable
+  wr_ena       : in  std_logic;
+  --! Write data input
+  wr_din       : in  std_logic_vector(FIFO_WIDTH-1 downto 0);
+  --! FIFO full
+  wr_full      : out std_logic;
+  --! FIFO almost full
+  wr_alm_full  : out std_logic;
+  --! FIFO overflow (wr_ena=1 and wr_full=1)
+  wr_overflow  : out std_logic;
+  --! Read clock
+  rd_clk       : in  std_logic;
+  --! Read request/acknowledge
+  rd_req_ack   : in  std_logic;
+  --! Read data output
+  rd_dout      : out std_logic_vector(FIFO_WIDTH-1 downto 0);
+  --! FIFO empty
+  rd_empty     : out std_logic;
+  --! FIFO almost empty
+  rd_alm_empty : out std_logic;
+  --! FIFO underflow (rd_req_ack=1 and rd_empty=1)
+  rd_underflow : out std_logic
+);
+begin
+  ASSERT ALMOST_FULL_THRESHOLD<FIFO_DEPTH
+    REPORT "Almost full threshold must be smaller than FIFO depth."
+    SEVERITY Error;
+  ASSERT ALMOST_EMPTY_THRESHOLD<FIFO_DEPTH
+    REPORT "Almost empty threshold must be smaller than FIFO depth."
+    SEVERITY Error;
+end entity;
