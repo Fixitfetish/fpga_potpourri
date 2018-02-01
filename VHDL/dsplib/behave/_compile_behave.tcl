88dbdf65220ca5f5b45a93538f3cc4ed3658b57e
Fixitfetish <Fixitfetish@github>
2018-02-01 22:54:01 +0100

    corrections

--- VHDL/dsplib/behave/_compile_behave.tcl
+++ VHDL/dsplib/behave/_compile_behave.tcl
@@ -2,23 +2,17 @@
 # This script compiles all behavioral architectures of the DSP Library.
 # It is required to compile the generic DSP entities first !
 
-# path/location of this script
-set SCRIPTPATH [ file dirname [dict get [ info frame 0 ] file ] ]
-
 # library name
 set LIB "dsplib"
 set VHDL 1993
 
+# path/location of this script
+set BEHAVE_PATH [ file dirname [dict get [ info frame 0 ] file ] ]
+
 # create file list (with list compilation is faster)
 set filelist [list]
 
-lappend filelist $SCRIPTPATH/delay_dsp/delay_dsp.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult1_accu/signed_mult1_accu.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult2_accu/signed_mult2_accu.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult2_sum/signed_mult2_sum.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult/signed_mult.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult_accu/signed_mult_accu.behave.vhdl
-lappend filelist $SCRIPTPATH/signed_mult_sum/signed_mult_sum.behave.vhdl
+source $BEHAVE_PATH/_filelist.tcl 
 
 # compile file list
 if {[string equal $VHDL 2008]} {
---
+++ VHDL/dsplib/behave/_filelist.tcl
@@ -0,0 +1,21 @@
+# Append the generic DSPLIB Library file list with the behavioral architectures.
+# It is required to create the file list of all the generic DSP entities first !
+
+# path/location of this script
+set BEHAVE_PATH [ file dirname [dict get [ info frame 0 ] file ] ]
+
+# create local file list
+set files [list]
+
+# stratixv specific
+lappend files signed_mult1_accu.behave.vhdl
+lappend files signed_mult2_accu.behave.vhdl
+lappend files signed_mult2_sum.behave.vhdl
+lappend files signed_mult_accu.behave.vhdl
+lappend files signed_mult_sum.behave.vhdl
+lappend files signed_mult.behave.vhdl
+
+# append files to existing file list
+foreach f $files {
+  lappend filelist [file normalize "${BEHAVE_PATH}/$f"]
+}
