f8e72646449669ced53754ff295f1245fb05db28
Fixitfetish <Fixitfetish@github.com>
2019-03-21 23:03:51 +0100

    preliminary fifo_logic_sync

--- VHDL/ramlib/fifo_logic_sync2.vhdl
+++ VHDL/ramlib/fifo_logic_sync2.vhdl
@@ -1,8 +1,8 @@
 -------------------------------------------------------------------------------
 --! @file       fifo_logic_sync2.vhdl
 --! @author     Fixitfetish
---! @date       20/Mar/2019
---! @version    0.30
+--! @date       21/Mar/2019
+--! @version    0.31
 --! @note       VHDL-1993
 --! @copyright  <https://en.wikipedia.org/wiki/MIT_License> ,
 --!             <https://opensource.org/licenses/MIT>
@@ -12,7 +12,8 @@
 -- HISTORY
 -- 0.10 : 28/May/2016  First version
 -- 0.20 : 18/Mar/2019  FIFO depth and thresholds reconfigurable during reset
--- 0.30 : 20/Mar/2019  New generic to define reset value of FIFO full flag
+-- 0.30 : 20/Mar/2019  New generic to define reset value of FIFO full and prog_full flags
+-- 0.31 : 21/Mar/2019  Report errors also during reset.  Minor changes and improved comments.
 -------------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -58,31 +59,33 @@ port (
   --! @brief FIFO depth in number of data words minus 1 (mandatory!). (2**N)-1 is recommended for efficiency.
   --! Can only be changed during reset.
   cfg_fifo_depth_minus1    : unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0);
-  --! @brief 0(unused) < prog full threshold <= cfg_fifo_depth_minus1
-  --! Can only be changed during reset.
+  --! @brief 0(unused) < prog full threshold <= cfg_fifo_depth_minus1 .
+  --! Can only be changed during reset. Optional, by default unused.
   cfg_prog_full_threshold  : unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0) := (others=>'0');
-  --! @brief 0(unused) < prog empty threshold <= cfg_fifo_depth_minus1
-  --! Can only be changed during reset.
+  --! @brief 0(unused) < prog empty threshold <= cfg_fifo_depth_minus1 .
+  --! Can only be changed during reset. Optional, by default unused.
   cfg_prog_empty_threshold : unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0) := (others=>'0');
   --! Write data enable
   wr_ena                   : in  std_logic;
   --! Write pointer for RAM based FIFOs with range 0 to cfg_fifo_depth_minus1
   wr_ptr                   : out unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0);
-  --! FIFO full, '1' when level = FIFO depth
+  --! FIFO full, '1' when level = FIFO depth or during reset when FULL_RESET_VALUE='1'
   wr_full                  : out std_logic;
-  --! FIFO programmable full, '1' when level>=cfg_prog_full_threshold
+  --! @brief FIFO programmable full, '1' when level>=cfg_prog_full_threshold .
+  --! By default returns FULL_RESET_VALUE when unused and during reset.
   wr_prog_full             : out std_logic;
-  --! FIFO overflow (when wr_ena=1 and wr_full=1)
+  --! FIFO overflow (one cycle after wr_ena=1 and wr_full=1)
   wr_overflow              : out std_logic;
   --! Read data enable
   rd_ena                   : in  std_logic;
   --! Read pointer for RAM based FIFOs with range 0 to cfg_fifo_depth_minus1
   rd_ptr                   : out unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0);
-  --! FIFO empty, '1' when level = 0
+  --! FIFO empty, '1' when level = 0 or during reset
   rd_empty                 : out std_logic;
-  --! FIFO programmable empty, '1' when level<=cfg_prog_empty_threshold
+  --! @brief FIFO programmable empty, '1' when level<=cfg_prog_empty_threshold .
+  --! By default returns '1' when unused and during reset.
   rd_prog_empty            : out std_logic;
-  --! FIFO underflow (when rd_ena=1 and rd_empty=1)
+  --! FIFO underflow (one cycle after rd_ena=1 and rd_empty=1)
   rd_underflow             : out std_logic;
   --! FIFO fill level with range 0 to FIFO depth
   level                    : out unsigned(MAX_FIFO_DEPTH_LOG2 downto 0)
@@ -92,6 +95,8 @@ end entity;
 -------------------------------------------------------------------------------
 
 architecture rtl of fifo_logic_sync2 is
+
+  constant EMPTY_RESET_VALUE : std_logic := '1';
 
   signal cfg_fifo_depth_minus1_q : unsigned(MAX_FIFO_DEPTH_LOG2-1 downto 0);
   signal cfg_fifo_depth_q : unsigned(MAX_FIFO_DEPTH_LOG2 downto 0);
@@ -115,14 +120,12 @@ begin
 
       if rst='1' then
         v_level := (others=>'0');
-        empty_i <= '1';
         full_i <= FULL_RESET_VALUE;
+        empty_i <= EMPTY_RESET_VALUE;
         wr_ptr_i <= (others=>'0');
         rd_ptr_i <= (others=>'0');
         wr_prog_full <= FULL_RESET_VALUE;
-        rd_prog_empty <= '1';
-        wr_overflow <= '0';
-        rd_underflow <= '0';
+        rd_prog_empty <= EMPTY_RESET_VALUE;
         cfg_fifo_depth_minus1_q <= cfg_fifo_depth_minus1;
         cfg_fifo_depth_q <= resize(cfg_fifo_depth_minus1,cfg_fifo_depth_q'length) + 1;
         cfg_prog_full_threshold_q <= cfg_prog_full_threshold;
@@ -130,10 +133,7 @@ begin
 
       else
 
-        wr_overflow <= wr_ena and full_i;
         v_incr := wr_ena and (not full_i); 
-
-        rd_underflow <= rd_ena and empty_i;
         v_decr := rd_ena and (not empty_i); 
 
         -- increment/decrement level
@@ -168,7 +168,7 @@ begin
 
         if cfg_prog_empty_threshold_q=0 then
           -- unused, keep reset value
-          rd_prog_empty <= '1';
+          rd_prog_empty <= EMPTY_RESET_VALUE;
         else
           rd_prog_empty <= to_01(v_level<=cfg_prog_empty_threshold_q);
         end if;
@@ -181,8 +181,12 @@ begin
         end if;
 
       end if;
-      
+
       level <= v_level;
+
+      -- report errors (also during reset)
+      wr_overflow <= wr_ena and full_i;
+      rd_underflow <= rd_ena and empty_i;
 
     end if; --clock
   end process;
