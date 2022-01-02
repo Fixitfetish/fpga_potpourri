9f059763f8ba54aa324ab9dc08bc6c3525d20130
Fixitfetish <Fixitfetish@github.com>
2022-01-02 21:37:18 +0100

    more complex multiplier stuff

--- VHDL/dsplib/dsp58/complex_mult1add1.dsp58.vhdl
+++ VHDL/dsplib/dsp58/complex_mult1add1.dsp58.vhdl
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
---! @file       complex_mult1_accu.dsp58.vhdl
+--! @file       complex_mult1add1.dsp58.vhdl
 --! @author     Fixitfetish
 --! @date       29/Dec/2021
 --! @version    0.00-draft
@@ -21,24 +21,35 @@ library dsplib;
 library unisim;
   use unisim.vcomponents.all;
 
---! @brief This is an implementation of the entity complex_mult1_accu for Xilinx Versal.
+--! @brief This is an implementation of the entity complex_mult1add1 for Xilinx Versal.
 --! One complex multiplication is performed and results are accumulated.
 --!
 --! Refer to Xilinx Versal ACAP DSP Engine, Architecture Manual, AM004 (v1.1.2) July 15, 2021
 --!
---! @image html complex_mult1_accu.dsp58.svg "" width=600px
+--! @image html complex_mult1add1.dsp58.svg "" width=600px
 --!
 --! **MAXIMUM_PERFORMANCE**
 --! * This implementation requires two back-to-back DSP58s.
 --! * Chaining is supported.
---! * The number of overall pipeline stages is typically NUM_INPUT_REG + 1 + NUM_OUTPUT_REG.
+--! * The number of overall pipeline stages is typically NUM_INPUT_REG_XY + NUM_OUTPUT_REG.
 --! 
-architecture dsp58 of complex_mult1_accu is
+architecture dsp58 of complex_mult1add1 is
 
   -- identifier for reports of warnings and errors
-  constant IMPLEMENTATION : string := "complex_mult1_accu(dsp58)";
+  constant IMPLEMENTATION : string := "complex_mult1add1(dsp58)";
+
+  -- two data input registers are supported, the first and the third stage
+  function ABREG(n:natural) return natural is
+  begin 
+    if    n<=1 then return n;
+    elsif n=2  then return 1; -- second input register uses MREG
+    else            return 2;
+    end if;
+  end function;
+
 
   -- derived constants
+  constant ACCU_RND_DISABLE : boolean := USE_CHAIN_INPUT and USE_Z_INPUT;
   constant ROUND_ENABLE : boolean := OUTPUT_ROUND and (OUTPUT_SHIFT_RIGHT/=0);
   constant PRODUCT_WIDTH : natural := x_re'length + 1 + y_re'length;
   constant MAX_GUARD_BITS : natural := ACCU_WIDTH - PRODUCT_WIDTH;
@@ -60,7 +71,11 @@ architecture dsp58 of complex_mult1_accu is
     c_im : signed(MAX_WIDTH_C-1 downto 0);
   end record;
   signal dsp_in : r_dsp_in;
-  signal dsp_feed : r_dsp_in;
+
+  signal alumode : std_logic_vector(3 downto 0);
+  signal opmode : std_logic_vector(8 downto 0);
+
+  signal dsp_feed_re, dsp_feed_im : r_dsp_feed;
 
   signal chainin_re_i, chainout_re_i : std_logic_vector(ACCU_WIDTH-1 downto 0);
   signal chainin_im_i, chainout_im_i : std_logic_vector(ACCU_WIDTH-1 downto 0);
@@ -71,14 +86,14 @@ architecture dsp58 of complex_mult1_accu is
   signal reset : std_logic := '0';
   signal ofl_re , ofl_im : std_logic;
   signal ufl_re , ufl_im : std_logic;
-  signal result_ovf_re , result_ovf_im : std_logic;
+  signal r_ovf_re , r_ovf_im : std_logic;
 
 begin
 
   -- check input/output length
   assert (x_re'length<=18 and x_im'length<=18 and y_re'length<=18 and y_im'length<=18)
     report "ERROR " & IMPLEMENTATION & ": " & 
-           "Complex multiplier input width is limited to 18."
+           "Complex multiplier input width of X and Y is limited to 18."
     severity failure;
 
   assert GUARD_BITS_EVAL<=MAX_GUARD_BITS
@@ -92,6 +107,89 @@ begin
            "More guard bits required for saturation/clipping and/or overflow detection."
     severity failure;
 
+--  inmode(0) <= '0'; -- AREG controlled input
+--  inmode(1) <= '0'; -- do not gate A/B
+--  inmode(2) <= '1'; -- D into preadder
+--  inmode(3) <= '0'; -- logic_ireg(0).sub; -- +/- A
+--  inmode(4) <= '0'; -- BREG controlled input
+
+  alumode <= "0000"; -- always P = Z + (W + X + Y + CIN)
+
+  i_dsp_feed_re : entity work.dsp_input_logic_dsp58
+  generic map(
+    PIPEREGS_RST     => NUM_INPUT_REG_XY,
+    PIPEREGS_CLR     => NUM_INPUT_REG_XY,
+    PIPEREGS_VLD     => NUM_INPUT_REG_XY,
+    PIPEREGS_ALUMODE => NUM_INPUT_REG_XY - INMODEREG(NUM_INPUT_REG_XY),
+    PIPEREGS_INMODE  => open, -- unused
+    PIPEREGS_OPMODE  => NUM_INPUT_REG_XY - INMODEREG(NUM_INPUT_REG_XY),
+    PIPEREGS_A       => NUM_IREG(LOGIC,NUM_INPUT_REG_XY),
+    PIPEREGS_B       => NUM_IREG(LOGIC,NUM_INPUT_REG_XY),
+    PIPEREGS_C       => NUM_IREG_C(LOGIC,NUM_INPUT_REG_Z),
+    PIPEREGS_D       => open  -- unused
+  )
+  port map(
+    clk         => clk,
+    rst         => open,
+    clkena      => clkena,
+    src_rst     => rst,
+    src_clr     => clr,
+    src_vld     => vld,
+    src_alumode => alumode,
+    src_inmode  => open, -- unused
+    src_opmode  => open,
+    src_a       => x_re,
+    src_b       => y_re,
+    src_c       => z_re,
+    src_d       => "00", -- unused
+    dsp_feed    => dsp_feed_re
+  );
+
+  i_dsp_feed_im : entity work.dsp_input_logic_dsp58
+  generic map(
+    PIPEREGS_RST     => NUM_INPUT_REG_XY,
+    PIPEREGS_CLR     => NUM_INPUT_REG_XY,
+    PIPEREGS_VLD     => NUM_INPUT_REG_XY,
+    PIPEREGS_ALUMODE => NUM_INPUT_REG_XY - INMODEREG(NUM_INPUT_REG_XY),
+    PIPEREGS_INMODE  => open, -- unused
+    PIPEREGS_OPMODE  => NUM_INPUT_REG_XY - INMODEREG(NUM_INPUT_REG_XY),
+    PIPEREGS_A       => NUM_IREG(LOGIC,NUM_INPUT_REG_XY),
+    PIPEREGS_B       => NUM_IREG(LOGIC,NUM_INPUT_REG_XY),
+    PIPEREGS_C       => NUM_IREG_C(LOGIC,NUM_INPUT_REG_Z),
+    PIPEREGS_D       => open  -- unused
+  )
+  port map(
+    clk         => clk,
+    rst         => open,
+    clkena      => clkena,
+    src_rst     => rst,
+    src_clr     => clr,
+    src_vld     => vld,
+    src_alumode => alumode,
+    src_inmode  => open, -- unused
+    src_opmode  => open,
+    src_a       => x_im,
+    src_b       => y_im,
+    src_c       => z_im,
+    src_d       => "00", -- unused
+    dsp_feed    => dsp_feed_im
+  );
+
+  i_opmode : entity work.dsp_opmode_logic_dsp58
+  generic map(
+    NUM_INPUT_REG   => NUM_INPUT_REG_XY - INMODEREG(NUM_INPUT_REG_XY),
+    USE_CHAIN_INPUT => USE_CHAIN_INPUT,
+    USE_C_INPUT     => USE_Z_INPUT,
+    ENABLE_P_REG    => (NUM_OUTPUT_REG>=1)
+  )
+  port map(
+    clk     => clk,
+    rst     => rst,
+    clkena  => clkena,
+    clr     => clr,
+    vld     => vld,
+    opmode  => opmode
+  );
 
   -- use only LSBs of chain input
   chainin_re_i <= std_logic_vector(chainin_re(ACCU_WIDTH-1 downto 0));
@@ -102,10 +200,10 @@ begin
      ACASCREG_IM                  => 1, -- integer := 1;
      ACASCREG_RE                  => 1, -- integer := 1;
      ADREG                        => 1, -- integer := 1;
-     ALUMODEREG_IM                => 1, -- integer := 1;
-     ALUMODEREG_RE                => 1, -- integer := 1;
-     AREG_IM                      => 2, -- integer := 2;
-     AREG_RE                      => 2, -- integer := 2;
+     ALUMODEREG_IM                => INMODEREG(NUM_INPUT_REG_XY),
+     ALUMODEREG_RE                => INMODEREG(NUM_INPUT_REG_XY),
+     AREG_IM                      => ABREG(NUM_INPUT_REG_XY), -- integer := 2;
+     AREG_RE                      => ABREG(NUM_INPUT_REG_XY), -- integer := 2;
      AUTORESET_PATDET_IM          => "NO_RESET", -- string := "NO_RESET";
      AUTORESET_PATDET_RE          => "NO_RESET", -- string := "NO_RESET";
      AUTORESET_PRIORITY_IM        => "RESET", -- string := "RESET";
@@ -114,8 +212,8 @@ begin
      A_INPUT_RE                   => "DIRECT", -- string := "DIRECT";
      BCASCREG_IM                  => 1, -- integer := 1;
      BCASCREG_RE                  => 1, -- integer := 1;
-     BREG_IM                      => 2, -- integer := 2;
-     BREG_RE                      => 2, -- integer := 2;
+     BREG_IM                      => ABREG(NUM_INPUT_REG_XY), -- integer := 2;
+     BREG_RE                      => ABREG(NUM_INPUT_REG_XY), -- integer := 2;
      B_INPUT_IM                   => "DIRECT", -- string := "DIRECT";
      B_INPUT_RE                   => "DIRECT", -- string := "DIRECT";
      CARRYINREG_IM                => 1, -- integer := 1;
@@ -124,8 +222,8 @@ begin
      CARRYINSELREG_RE             => 1, -- integer := 1;
      CONJUGATEREG_A               => 1, -- integer := 1;
      CONJUGATEREG_B               => 1, -- integer := 1;
-     CREG_IM                      => 1, -- integer := 1;
-     CREG_RE                      => 1, -- integer := 1;
+     CREG_IM                      => NUM_IREG_C(DSP,NUM_INPUT_REG_Z), -- integer := 1;
+     CREG_RE                      => NUM_IREG_C(DSP,NUM_INPUT_REG_Z), -- integer := 1;
      IS_ALUMODE_IM_INVERTED       => (others=>'0'), -- std_logic_vector(3 downto 0) := "0000";
      IS_ALUMODE_RE_INVERTED       => (others=>'0'), -- std_logic_vector(3 downto 0) := "0000";
      IS_ASYNC_RST_INVERTED        => '0', -- bit := '0';
@@ -157,17 +255,17 @@ begin
      IS_RSTP_RE_INVERTED          => '0', -- bit := '0';
      MASK_IM                      => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"FFFFFFFFFFFFFF";
      MASK_RE                      => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"FFFFFFFFFFFFFF";
-     MREG_IM                      => 1, -- integer := 1;
-     MREG_RE                      => 1, -- integer := 1;
-     OPMODEREG_IM                 => 1, -- integer := 1;
-     OPMODEREG_RE                 => 1, -- integer := 1;
+     MREG_IM                      => MREG(NUM_INPUT_REG_XY),
+     MREG_RE                      => MREG(NUM_INPUT_REG_XY),
+     OPMODEREG_IM                 => INMODEREG(NUM_INPUT_REG_XY),
+     OPMODEREG_RE                 => INMODEREG(NUM_INPUT_REG_XY),
      PATTERN_IM                   => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"00000000000000";
      PATTERN_RE                   => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"00000000000000";
-     PREG_IM                      => 1, -- integer := 1;
-     PREG_RE                      => 1, -- integer := 1;
+     PREG_IM                      => PREG(NUM_OUTPUT_REG),
+     PREG_RE                      => PREG(NUM_OUTPUT_REG),
      RESET_MODE                   => "SYNC", -- string := "SYNC";
-     RND_IM                       => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"00000000000000";
-     RND_RE                       => (others=>'0'), -- std_logic_vector(57 downto 0) := "00" & X"00000000000000";
+     RND_IM                       => RND(ROUND_ENABLE and not ACCU_RND_DISABLE,OUTPUT_SHIFT_RIGHT), -- Rounding Constant
+     RND_RE                       => RND(ROUND_ENABLE and not ACCU_RND_DISABLE,OUTPUT_SHIFT_RIGHT), -- Rounding Constant
      SEL_MASK_IM                  => "MASK", -- string := "MASK";
      SEL_MASK_RE                  => "MASK", -- string := "MASK";
      SEL_PATTERN_IM               => "PATTERN", -- string := "PATTERN";
@@ -200,15 +298,15 @@ begin
      UNDERFLOW_RE      => ufl_re, -- out std_ulogic;
      ACIN_IM           => open, -- in std_logic_vector(17 downto 0);
      ACIN_RE           => open, -- in std_logic_vector(17 downto 0);
-     ALUMODE_IM        => "0000", -- in std_logic_vector(3 downto 0);
-     ALUMODE_RE        => "0000", -- in std_logic_vector(3 downto 0);
+     ALUMODE_IM        => dsp_feed_im.alumode, -- in std_logic_vector(3 downto 0);
+     ALUMODE_RE        => dsp_feed_re.alumode, -- in std_logic_vector(3 downto 0);
      ASYNC_RST         => open, -- in std_ulogic;
-     A_IM              => std_logic_vector(dsp_feed.a_im),
-     A_RE              => std_logic_vector(dsp_feed.a_re),
+     A_IM              => std_logic_vector(dsp_feed_im.a(17 downto 0)),
+     A_RE              => std_logic_vector(dsp_feed_re.a(17 downto 0)),
      BCIN_IM           => open, -- in std_logic_vector(17 downto 0);
      BCIN_RE           => open, -- in std_logic_vector(17 downto 0);
-     B_IM              => std_logic_vector(dsp_feed.b_im),
-     B_RE              => std_logic_vector(dsp_feed.b_re),
+     B_IM              => std_logic_vector(dsp_feed_im.b(17 downto 0)),
+     B_RE              => std_logic_vector(dsp_feed_re.b(17 downto 0)),
      CARRYCASCIN_IM    => open, -- in std_ulogic;
      CARRYCASCIN_RE    => open, -- in std_ulogic;
      CARRYINSEL_IM     => open, -- in std_logic_vector(2 downto 0);
@@ -241,12 +339,12 @@ begin
      CLK               => clk, -- in std_ulogic;
      CONJUGATE_A       => '0', -- in std_ulogic;
      CONJUGATE_B       => '0', -- in std_ulogic;
-     C_IM              => std_logic_vector(dsp_feed.c_im),
-     C_RE              => std_logic_vector(dsp_feed.c_re),
+     C_IM              => std_logic_vector(dsp_feed_im.c),
+     C_RE              => std_logic_vector(dsp_feed_re.c),
      MULTSIGNIN_IM     => open, -- in std_ulogic;
      MULTSIGNIN_RE     => open, -- in std_ulogic;
-     OPMODE_IM         => dsp_feed.opmode,
-     OPMODE_RE         => dsp_feed.opmode,
+     OPMODE_IM         => opmode,
+     OPMODE_RE         => opmode,
      PCIN_IM           => chainin_im_i, -- in std_logic_vector(57 downto 0);
      PCIN_RE           => chainin_re_i, -- in std_logic_vector(57 downto 0);
      RSTAD             => reset, -- in std_ulogic;
@@ -287,13 +385,13 @@ begin
         if rst/='0' then
           accu_vld <= '0';
         elsif clkena='1' then
-          accu_vld <= dsp_feed.vld;
+          accu_vld <= dsp_feed_re.vld;
         end if;
       end if;
     end process;
   end generate;
   g_dspreg_off : if NUM_OUTPUT_REG<=0 generate
-    accu_vld <= dsp_feed.vld;
+    accu_vld <= dsp_feed_re.vld;
   end generate;
 
   -- cut off unused sign extension bits
@@ -307,7 +405,7 @@ begin
   generic map(
     PIPELINE_STAGES    => NUM_OUTPUT_REG-1,
     OUTPUT_SHIFT_RIGHT => OUTPUT_SHIFT_RIGHT,
-    OUTPUT_ROUND       => OUTPUT_ROUND and USE_CHAIN_INPUT,
+    OUTPUT_ROUND       => ROUND_ENABLE and ACCU_RND_DISABLE,
     OUTPUT_CLIP        => OUTPUT_CLIP,
     OUTPUT_OVERFLOW    => OUTPUT_OVERFLOW
   )
@@ -319,14 +417,14 @@ begin
     dsp_out_vld => accu_vld,
     result      => result_re,
     result_vld  => result_vld,
-    result_ovf  => result_ovf_re
+    result_ovf  => r_ovf_re
   );
 
   i_out_im : entity dsplib.signed_output_logic
   generic map(
     PIPELINE_STAGES    => NUM_OUTPUT_REG-1,
     OUTPUT_SHIFT_RIGHT => OUTPUT_SHIFT_RIGHT,
-    OUTPUT_ROUND       => OUTPUT_ROUND and USE_CHAIN_INPUT,
+    OUTPUT_ROUND       => ROUND_ENABLE and ACCU_RND_DISABLE,
     OUTPUT_CLIP        => OUTPUT_CLIP,
     OUTPUT_OVERFLOW    => OUTPUT_OVERFLOW
   )
@@ -338,10 +436,11 @@ begin
     dsp_out_vld => accu_vld,
     result      => result_im,
     result_vld  => open, -- same as real
-    result_ovf  => result_ovf_im
+    result_ovf  => r_ovf_im
   );
 
   -- TODO
-  result_ovf <= result_ovf_re or result_ovf_im or ofl_re or ofl_im or ufl_re or ufl_im;
+  result_ovf_re <= r_ovf_re or ofl_re or ufl_re;
+  result_ovf_im <= r_ovf_im or ofl_im or ufl_im;
 
 end architecture;
